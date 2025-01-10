733114094e4e8a928f7e39bb76a45b0279ae7d99
fu941110 <fu941110@gapp.nthu.edu.tw>
2025-01-10 15:40:06 +0800

    second commit

--- test.cpp
+++ test.cpp
@@ -6,12 +6,38 @@ using namespace std;
 
 const int MAXN = 100005;
 
-void main()
-{
-    int n;
-    cin >> n;
+vector<int> g[MAXN];
 
-    cout << (n * (n + 1) * (2 * n + 1)) / 6 << endl;
+int n, m;
 
-    
+bool vis[MAXN];
+
+int dfs(int u) {
+    vis[u] = true;
+    int cnt = 1;
+    for (int v : g[u]) {
+        if (!vis[v]) {
+            cnt += dfs(v);
+        }
+    }
+    return cnt;
 }
+
+    int main() {
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i]) {
+                ans = max(ans, dfs(i));
+            }
+        }
+        cout << ans << endl;
+        return 0;
+    }
+    
