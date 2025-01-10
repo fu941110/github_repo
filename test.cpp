89c015f2aad4b87bb29eb1066be9ab41e576b093
fu941110 <fu941110@gapp.nthu.edu.tw>
2025-01-10 15:07:32 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+const int MAXN = 100005;
+
+void main()
+{
+    int n;
+    cin >> n;
+
+    cout << (n * (n + 1) * (2 * n + 1)) / 6 << endl;
+
+    
+}
